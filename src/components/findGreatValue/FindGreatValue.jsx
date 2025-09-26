df2414c048400da1f4394b363f12d68bd629cd2f
Deepu Singh <159556385+1Lover0@users.noreply.github.com>
2025-09-26 13:41:05 +0530

    section adding

--- src/components/findGreatValue/FindGreatValue.jsx
+++ src/components/findGreatValue/FindGreatValue.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
+import './FindGreatValue.css'
 
 const FindGreatValue = () => {
   return (
     <div className='findgreatvalue'>
         <h2>Helping you find greate value</h2>
+        <div className="findgreatvalue-items">
+
+          <div className="findgreatvalue-item">
+            <div className="findgreatvalue-item-header">
+              <img src="" alt="" />
+              <p>Fruit & Veg</p>
+            </div>
+            <div className="findgreatvalue-item-center">
+              <img src="" alt="" />
+              <p> Save at least 20%* with The Odd Bunch. T&Cs apply</p>
+            </div>
+            <button className='findgreatvalue-item-button'>Shop now</button>
+          </div>
+          
+          <div className="findgreatvalue-item">
+            <div className="findgreatvalue-item-header">
+              <img src="" alt="" />
+              <p>Fruit & Veg</p>
+            </div>
+            <div className="findgreatvalue-item-center">
+              <img src="" alt="" />
+              <p> Sizzling burger deals</p>
+            </div>
+            <button className='findgreatvalue-item-button'>Shop now</button>
+          </div>
+          
+          <div className="findgreatvalue-item">
+            <div className="findgreatvalue-item-header">
+              <img src="" alt="" />
+              <p>Fruit & Veg</p>
+            </div>
+            <div className="findgreatvalue-item-center">
+              <img src="" alt="" />
+              <p> Save at least 20%* with The Odd Bunch. T&Cs apply</p>
+            </div>
+            <button className='findgreatvalue-item-button'>Shop now</button>
+          </div>
+          
+          <div className="findgreatvalue-item">
+            <div className="findgreatvalue-item-header">
+              <img src="" alt="" />
+              <p>Fruit & Veg</p>
+            </div>
+            <div className="findgreatvalue-item-center">
+              <img src="" alt="" />
+              <p> Save at least 20%* with The Odd Bunch. T&Cs apply</p>
+            </div>
+            <button className='findgreatvalue-item-button'>Shop now</button>
+          </div>
+          
+          <div className="findgreatvalue-item">
+            <div className="findgreatvalue-item-header">
+              <img src="" alt="" />
+              <p>Fruit & Veg</p>
+            </div>
+            <div className="findgreatvalue-item-center">
+              <img src="" alt="" />
+              <p> Save at least 20%* with The Odd Bunch. T&Cs apply</p>
+            </div>
+            <button className='findgreatvalue-item-button'>Shop now</button>
+          </div>
+          
+          
+        </div>
     </div>
   )
 }
